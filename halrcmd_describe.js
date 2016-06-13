782c7aafb6ea5cda173a9f9f8e20eb3399d46beb
Alexander Rössler <mail.aroessler@gmail.com>
2016-06-13 15:49:58 +0200

    added tests

---
+++ halrcmd_describe.js
@@ -0,0 +1,17 @@
+var EventEmitter = require('events').EventEmitter;
+var zmq = require('zmq');
+var protobufMessage = require('machinetalk-protobuf').message;
+var Container = protobufMessage.Container;
+var ContainerType = protobufMessage.ContainerType;
+
+function socketMessageReceived(rx) {
+    rx = Container.decode(rx);
+    console.log('received message:');
+    console.log(rx);
+}
+
+var socket = zmq.socket('router');
+socket.setsockopt(zmq.ZMQ_LINGER, 0);
+socket.bind('tcp://127.0.0.1:12345');
+socket.on('message', socketMessageReceived);
+
